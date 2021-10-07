86e4a8d30b22fb839797f239d81c60b6e2767a1c
Reddy, Sidda <Sidda.Reddy@emc.com>
2021-10-07 14:35:37 +0530

    Read file list pass and fail students

--- ReadFile-P-pass-F-Fail.py
+++ ReadFile-P-pass-F-Fail.py
@@ -5,7 +5,7 @@ count=0
 for line in f:
     line_split=line.split()
     if line_split[2] == 'P':
-        print(line)
+        print(str(count)+ ' ' +line)
     #print(str(count)+ line)
     #count=count+1
 f.close()
